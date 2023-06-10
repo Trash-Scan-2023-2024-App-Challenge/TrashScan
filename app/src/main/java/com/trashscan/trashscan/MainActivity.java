4488d325c2b308db7601dd299c6d8c10f85d1da8
green60ninja <82245459+green60ninja@users.noreply.github.com>
2023-06-10 01:05:54 -0500

    Created basic login page and added code to handle getting username and password.

--- app/src/main/java/com/trashscan/trashscan/MainActivity.java
+++ app/src/main/java/com/trashscan/trashscan/MainActivity.java
@@ -1,8 +1,12 @@
 package com.trashscan.trashscan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.logInButton);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String u = username.getText().toString();
+                String p = password.getText().toString();
+                TextView t = findViewById(R.id.textView1);
+                t.setText("USERNAME: "+u + "\nPASSWORD: "+p);
+                // For testing, outputs username & pass. to random textview
+                // TODO: Do something with this?
+            }
+        });
     }
 }
