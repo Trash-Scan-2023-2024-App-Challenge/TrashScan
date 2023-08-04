da5752f458c2aa508bff12520e52dbc6bed04727
green60ninja <82245459+green60ninja@users.noreply.github.com>
2023-08-03 20:33:05 -0500

    Added functionality to EditLayoutActivity NOTE: Semi-dangerous when directly changing email or password

--- app/src/main/java/com/trashscan/trashscan/EditLayoutActivity.java
+++ app/src/main/java/com/trashscan/trashscan/EditLayoutActivity.java
@@ -2,27 +2,59 @@ package com.trashscan.trashscan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class EditLayoutActivity extends AppCompatActivity {
 
-    private Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_layout);
-        backButton = (Button) findViewById(R.id.saveButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        Button saveButton = findViewById(R.id.saveButton);
+        EditText emailEdit = findViewById(R.id.email_edit);
+        EditText oldPasswordEdit = findViewById(R.id.oldpassword_edit);
+        EditText newPasswordEdit = findViewById(R.id.newpassword_edit);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        backButton.setOnClickListener(view -> backActivity());
+        // TODO: Make functionality for editing user information.
+        // Use mAuth.getCurrentUser().updateEmail(email);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                backActivity();
+            public void onClick(View v) {
+                try {
+                    if (!emailEdit.getText().toString().isEmpty()) {
+                        mAuth.getCurrentUser().updateEmail(emailEdit.getText().toString());
+                        Log.d("DEBUG:", emailEdit.getText().toString());
+                    }
+
+
+                    if (!oldPasswordEdit.getText().toString().isEmpty() &&
+                            !newPasswordEdit.getText().toString().isEmpty()) {
+                        mAuth.getCurrentUser().updatePassword(newPasswordEdit.getText().toString());
+                    }
+                    Intent i = new Intent(getApplicationContext(),ProfileActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e) {
+                    Log.e("Error:",e.toString());
+                    Toast.makeText(EditLayoutActivity.this, "An Error Occurred", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
     }
-    public void backActivity(){
+
+    public void backActivity() {
         Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
         startActivity(intent);
         finish();
--- app/src/main/java/com/trashscan/trashscan/ProfileActivity.java
+++ app/src/main/java/com/trashscan/trashscan/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +19,8 @@ public class ProfileActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.password_change);
         Button signOut = (Button) findViewById(R.id.logout);
+        TextView email = findViewById(R.id.username);
+        email.setText("\nUsername: "+FirebaseAuth.getInstance().getCurrentUser().getEmail()+"\n");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,7 +37,7 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
     public void openEditActivity(){
-//        Intent intent = new Intent(this, EditLayoutActivity.class);
-//        startActivity(intent);
+        Intent intent = new Intent(this, EditLayoutActivity.class);
+        startActivity(intent);
     }
 }
